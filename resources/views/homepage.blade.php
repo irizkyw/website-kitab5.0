ea9b1f4842ce5a38e81d18da2cb9f3e39a9e0d59
velmanharefa <velmanh24@gmail.com>
2024-03-01 23:22:36 +0700

    test

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,21 +2,54 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Homepage</title>
+    <title>Homepage</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-        <!-- Styles -->
-        <style>
-        </style>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <!-- Styles -->
+    <style>
+    </style>
 </head>
 <body>
-  <p>Hello world</p>
+  <section class="navbar">
+    <!-- <div class="container-fluid border border-danger">
+      <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Scripture</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Record</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Contacts</a>
+        </li>
+      </ul>
+    </div> -->
+
+    <div class="container-fluid border border-danger">
+      <div class="row align-item-center">
+        <div class="col-sm-3 border border-danger">
+          <p>Do'a</p>
+        </div>
+        <div class="col-sm-9 border border-danger">
+          <p>Vio</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="body">
+  </section>
+
+
 </body>
 </html>
