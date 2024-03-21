cedd1ef42ba12346ba820a0785ac26199355491b
Darma Cahya <darmady6@gmail.com>
2024-03-21 17:07:41 +0700

    Update table name
    Update table name and and update Kitabs model to Books

--- database/migrations/2024_03_08_134443_create_kitabs_table.php
+++ database/migrations/2024_03_08_134443_create_kitabs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('kitabs', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('kitab')->nullable();
             $table->string('agama')->nullable();
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('kitabs');
+        Schema::dropIfExists('books');
     }
 };
