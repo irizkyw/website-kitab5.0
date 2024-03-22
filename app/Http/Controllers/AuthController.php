a12d18e5bae897854b4fb994dc194e7bf0d3ce00
irizkyw <ichwanrizkywahyudin3@gmail.com>
2024-03-22 23:50:51 +0700

    UPDATE: OAuth(Login System)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
